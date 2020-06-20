15f383c9eb09d59963122a43197742dd42ab4d67
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-20 17:55:30 +0500

    Add test

--- Tests/ValidationTests/Constraint/NotBlankConstraintTests.swift
+++ Tests/ValidationTests/Constraint/NotBlankConstraintTests.swift
@@ -9,4 +9,15 @@ final class NotBlankConstraintTestCase: XCTestCase {
         // Assert
         XCTAssertEqual(constraint.message, "This value must not be blank.")
     }
+
+    func testInitWithCustomMessage() {
+        // Arrange
+        let message = "This value is required."
+
+        // Act
+        let constraint = NotBlankConstraint(message: message)
+
+        // Assert
+        XCTAssertEqual(constraint.message, message)
+    }
 }
