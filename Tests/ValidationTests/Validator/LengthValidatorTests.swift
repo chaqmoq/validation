a3ba82aef4dd4572b45f2d54a698156b2b4b211f
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-20 18:16:11 +0500

    Add test

---
+++ Tests/ValidationTests/Validator/LengthValidatorTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import Validation
+
+final class LengthValidatorTests: XCTestCase {
+    var validator: ConstraintValidator!
+
+    override func setUp() {
+        super.setUp()
+
+        validator = LengthValidator()
+    }
+
+    func testValueAgainstImplicitConstraint() {
+        // Arrange
+        let value = "a"
+
+        // Act/Assert
+        XCTAssertThrowsError(try validator.validate(value)) { error in
+            XCTAssertTrue(error is ValidatorError)
+        }
+    }
+}
