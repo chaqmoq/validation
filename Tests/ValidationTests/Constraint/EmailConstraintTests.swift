491f6e4a5648857948d83dfda36a0dd45a750217
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-20 18:04:38 +0500

    Add test

--- Tests/ValidationTests/Constraint/EmailConstraintTests.swift
+++ Tests/ValidationTests/Constraint/EmailConstraintTests.swift
@@ -9,4 +9,15 @@ final class EmailConstraintTests: XCTestCase {
         // Assert
         XCTAssertEqual(constraint.message, "This value must be a valid email address.")
     }
+
+    func testInitWithCustomMessage() {
+        // Arrange
+        let message = "This value is not a valid email address."
+
+        // Act
+        let constraint = EmailConstraint(message: message)
+
+        // Assert
+        XCTAssertEqual(constraint.message, message)
+    }
 }
