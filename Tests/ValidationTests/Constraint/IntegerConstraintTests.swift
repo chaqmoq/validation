ad1c4e5f96ec4983fbeca6ab57046c4c7717907a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-20 17:47:19 +0500

    Add test

--- Tests/ValidationTests/Constraint/IntegerConstraintTests.swift
+++ Tests/ValidationTests/Constraint/IntegerConstraintTests.swift
@@ -64,4 +64,27 @@ final class IntegerConstraintTests: XCTestCase {
         XCTAssertEqual(constraint.maxMessage, "This value is too big. It must be less than or equal to \(max).")
         XCTAssertEqual(constraint.exactMessage, "This value must be \(min).")
     }
+
+    func testInitWithCustomMessages() {
+        // Arrange
+        let min: Int = 1
+        let max: Int = 2
+        let minMessage = "This value must have \(min) characters or more."
+        let maxMessage = "This value must have \(max) characters or less."
+        let exactMessage = "This value must exactly have 2 characters."
+
+        // Act
+        let constraint = IntegerConstraint(
+            min: min,
+            max: max,
+            minMessage: minMessage,
+            maxMessage: maxMessage,
+            exactMessage: exactMessage
+        )
+
+        // Assert
+        XCTAssertEqual(constraint.minMessage, minMessage)
+        XCTAssertEqual(constraint.maxMessage, maxMessage)
+        XCTAssertEqual(constraint.exactMessage, exactMessage)
+    }
 }
