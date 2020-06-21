8925c979e4d52a98a0f99bb32cd1c01207aa004b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-21 22:35:21 +0500

    Add test

--- Tests/ValidationTests/ConstraintTypeTests.swift
+++ Tests/ValidationTests/ConstraintTypeTests.swift
@@ -37,4 +37,29 @@ final class ConstraintTypeTests: XCTestCase {
         XCTAssertEqual(constraint.maxMessage, maxMessage)
         XCTAssertEqual(constraint.exactMessage, exactMessage)
     }
+
+    func testLengthType() {
+        // Arrange
+        let min: UInt = 1
+        let max: UInt = 2
+        let minMessage = "This value must have \(min) characters or more."
+        let maxMessage = "This value must have \(max) characters or less."
+        let exactMessage = "This value must exactly have 2 characters."
+
+        // Act
+        let constraint = ConstraintType.length(
+            min: min,
+            max: max,
+            minMessage: minMessage,
+            maxMessage: maxMessage,
+            exactMessage: exactMessage
+        ).constraint as! LengthConstraint
+
+        // Assert
+        XCTAssertEqual(constraint.min, min)
+        XCTAssertEqual(constraint.max, max)
+        XCTAssertEqual(constraint.minMessage, minMessage)
+        XCTAssertEqual(constraint.maxMessage, maxMessage)
+        XCTAssertEqual(constraint.exactMessage, exactMessage)
+    }
 }
