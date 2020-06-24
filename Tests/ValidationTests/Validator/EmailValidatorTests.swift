e660ca69e3eb2545a25264d639785e6a08a5e3fc
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-24 09:24:27 +0500

    Add test

---
+++ Tests/ValidationTests/Validator/EmailValidatorTests.swift
@@ -0,0 +1,22 @@
+import XCTest
+@testable import Validation
+
+final class EmailValidatorTests: XCTestCase {
+    var validator: EmailValidator!
+
+    override func setUp() {
+        super.setUp()
+
+        validator = EmailValidator()
+    }
+
+    func testEmptyValueWithImplicitConstraint() {
+        // Arrange
+        let value = ""
+
+        // Act/Assert
+        XCTAssertThrowsError(try validator.validate(value)) { error in
+            XCTAssertTrue(error is ConstraintViolation)
+        }
+    }
+}
