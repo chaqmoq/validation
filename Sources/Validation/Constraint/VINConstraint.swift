77748f33ac235cc4f26f16cd393b80169a0f1ed0
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-14 21:54:23 +0500

    Add documentation to VINConstraint

--- Sources/Validation/Constraint/VINConstraint.swift
+++ Sources/Validation/Constraint/VINConstraint.swift
@@ -1,9 +1,17 @@
+/// A constraint to validate if a value is a valid VIN.
 public struct VINConstraint: Constraint {
+    /// A default error message to show if no custom error message is provided.
     public static let message = "This value must be a valid VIN."
 
+    /// A custom error message.
     public let message: String
+
+    /// A validator named `VINValidator` to validate a value.
     public let validator: ConstraintValidator = VINValidator()
 
+    /// Initializes a new instance with a custom error message.
+    ///
+    /// - Parameter message: A custom error message. Defaults to a default error message.
     public init(message: String = message) {
         self.message = message
     }
