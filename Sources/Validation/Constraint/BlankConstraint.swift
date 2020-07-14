f18c0db1692e0abe517479fe9091e29710fa105a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-14 21:52:56 +0500

    Add documentation to BlankConstraint

--- Sources/Validation/Constraint/BlankConstraint.swift
+++ Sources/Validation/Constraint/BlankConstraint.swift
@@ -1,9 +1,17 @@
+/// A constraint to validate if a value is blank.
 public struct BlankConstraint: Constraint {
+    /// A default error message to show if no custom error message is provided.
     public static let message = "This value must be blank."
 
+    /// A custom error message.
     public let message: String
+
+    /// A validator named `BlankValidator` to validate a value.
     public let validator: ConstraintValidator = BlankValidator()
 
+    /// Initializes a new instance with a custom error message.
+    ///
+    /// - Parameter message: A custom error message. Defaults to a default error message.
     public init(message: String = message) {
         self.message = message
     }
