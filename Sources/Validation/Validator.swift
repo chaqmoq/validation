a82d1e314dfbd70e8349bb97b86a0fe30018af3c
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-14 22:55:26 +0500

    Add documentation to Validator

--- Sources/Validation/Validator.swift
+++ Sources/Validation/Validator.swift
@@ -1,3 +1,5 @@
+/// A default `Validator` class to validate a value with multiple types of `Constraint`s.
 public final class Validator: ConstraintValidator {
+    /// Initializes a new instance.
     public init() {}
 }
