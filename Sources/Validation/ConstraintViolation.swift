94c5dd59fb76b5f4b0ff8be130d0675f6e276395
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-14 22:55:05 +0500

    Add documentation to ConstraintViolation

--- Sources/Validation/ConstraintViolation.swift
+++ Sources/Validation/ConstraintViolation.swift
@@ -1,9 +1,16 @@
 import Foundation
 
+/// A common error type to throw by `ConstraintValidator` for an invalid value.
 public struct ConstraintViolation: LocalizedError {
+    /// A custom error message.
     public let message: String
+
+    /// See `LocalizedError`.
     public var errorDescription: String? { message }
 
+    /// Initializes a new instance with a custom error message.
+    ///
+    /// - Parameter message: A custom error message.
     public init(message: String) {
         self.message = message
     }
