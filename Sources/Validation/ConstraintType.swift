46b4600208fcb445ac9c7455c9ef892f85093bd3
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-14 22:54:22 +0500

    Add documentation to ConstraintType

--- Sources/Validation/ConstraintType.swift
+++ Sources/Validation/ConstraintType.swift
@@ -1,5 +1,18 @@
+/// A simplified API for the existing `Constraint`s.
 public enum ConstraintType {
+    /// Creates an `EmailConstraint`type with a custom error message.
+    ///
+    /// - Parameter message: A custom error message. Defaults to a default error message.
     case email(message: String = EmailConstraint.message)
+
+    /// Creates an `IntegerConstraint`type with minimum/maximum values and custom minimum/maximum error messages.
+    ///
+    /// - Parameters:
+    ///   - min: A custom minimum value. Defaults to a default minimum value.
+    ///   - max: A custom maximum value. Defaults to a default maximum value.
+    ///   - minMessage: A custom error message to show for a minimum value violation. Defaults to a default minimum value violation error message.
+    ///   - maxMessage: A custom error message to show for a maximum value violation. Defaults to a default maximum value violation error message.
+    ///   - exactMessage: A custom error message to show for an exact value violation. Defaults to a default exact value violation error message.
     case integer(
         min: Int = IntegerConstraint.min,
         max: Int = IntegerConstraint.max,
@@ -7,6 +20,15 @@ public enum ConstraintType {
         maxMessage: String = IntegerConstraint.maxMessage,
         exactMessage: String = IntegerConstraint.exactMessage
     )
+
+    /// Creates a `LengthConstraint` type with minimum/maximum values and custom minimum/maximum error messages.
+    ///
+    /// - Parameters:
+    ///   - min: A custom minimum value. Defaults to a default minimum value.
+    ///   - max: A custom maximum value. Defaults to a default maximum value.
+    ///   - minMessage: A custom error message to show for a minimum value violation. Defaults to a default minimum value violation error message.
+    ///   - maxMessage: A custom error message to show for a maximum value violation. Defaults to a default maximum value violation error message.
+    ///   - exactMessage: A custom error message to show for an exact value violation. Defaults to a default exact value violation error message.
     case length(
         min: UInt = LengthConstraint.min,
         max: UInt = LengthConstraint.max,
@@ -14,9 +36,18 @@ public enum ConstraintType {
         maxMessage: String = LengthConstraint.maxMessage,
         exactMessage: String = LengthConstraint.exactMessage
     )
+
+    /// Creates an `NotBlankConstraint` type with a custom error message.
+    ///
+    /// - Parameter message: A custom error message. Defaults to a default error message.
     case notBlank(message: String = NotBlankConstraint.message)
+
+    /// Creates an `VINConstraint` with a custom error message.
+    ///
+    /// - Parameter message: A custom error message. Defaults to a default error message.
     case vin(message: String = VINConstraint.message)
 
+    /// A matching `Constraint` for the `ConstraintType`.
     public var constraint: Constraint {
         switch self {
         case .email(let message):
