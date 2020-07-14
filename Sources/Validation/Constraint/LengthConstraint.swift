ae6d527874ae5d9900ab843fce64711f8408a528
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-07-14 21:53:42 +0500

    Add documentation to LengthConstraint

--- Sources/Validation/Constraint/LengthConstraint.swift
+++ Sources/Validation/Constraint/LengthConstraint.swift
@@ -1,26 +1,61 @@
+/// A constraint to validate if a value is with a valid length.
 public struct LengthConstraint: Constraint {
+    /// A default minimum value to set if no minimum value is provided.
     public static let min: UInt = 1
+
+    /// A default maximum value to set if no maximum value is provided.
     public static let max: UInt = UInt.max
+
+    /// A default error message to show for a minimum value violation.
     public static let minMessage = "This value is too short. It must have %d characters or more."
+
+    /// A default error message to show for a maximum value violation.
     public static let maxMessage = "This value is too long. It must have %d characters or less."
+
+    /// A default error message to show for an exact value violation.
     public static let exactMessage = "This value must have %d characters."
 
+    /// A custom minimum value.
     public let min: UInt
+
+    /// A custom maximum value.
     public let max: UInt
+
+    /// A custom error message to show for a minimum value violation.
     public let minMessage: String
+
+    /// A custom error message to show for a maximum value violation.
     public let maxMessage: String
+
+    /// A custom error message to show for an exact value violation.
     public let exactMessage: String
+
+    /// A validator named `LengthValidator` to validate a value.
     public let validator: ConstraintValidator = LengthValidator()
 
-    public init(
-        exact: UInt,
-        minMessage: String = minMessage,
-        maxMessage: String = maxMessage,
-        exactMessage: String = exactMessage
-    ) {
-        self.init(min: exact, max: exact, minMessage: minMessage, maxMessage: maxMessage, exactMessage: exactMessage)
+    /// Initializes a new instance with an exact value and custom error message.
+    ///
+    /// - Parameters:
+    ///   - exact: An exact value.
+    ///   - exactMessage: A custom error message to show for an exact value violation. Defaults to a default exact value violation error message.
+    public init(exact: UInt, exactMessage: String = exactMessage) {
+        self.init(
+            min: exact,
+            max: exact,
+            minMessage: LengthConstraint.minMessage,
+            maxMessage: LengthConstraint.maxMessage,
+            exactMessage: exactMessage
+        )
     }
 
+    /// Initializes a new instance with minimum/maximum values and custom minimum/maximum error messages.
+    ///
+    /// - Parameters:
+    ///   - min: A custom minimum value. Defaults to a default minimum value.
+    ///   - max: A custom maximum value. Defaults to a default maximum value.
+    ///   - minMessage: A custom error message to show for a minimum value violation. Defaults to a default minimum value violation error message.
+    ///   - maxMessage: A custom error message to show for a maximum value violation. Defaults to a default maximum value violation error message.
+    ///   - exactMessage: A custom error message to show for an exact value violation. Defaults to a default exact value violation error message.
     public init(
         min: UInt = min,
         max: UInt = max,
