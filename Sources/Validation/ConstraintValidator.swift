566d0d9593b5351e5cd4250d0d59b54040988ed9
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-21 11:07:47 +0500

    Update `ConstraintValidator` documentation

--- Sources/Validation/ConstraintValidator.swift
+++ Sources/Validation/ConstraintValidator.swift
@@ -1,17 +1,17 @@
-/// A common protocol for all validators.
+/// A common protocol for all `Constraint` validators.
 public protocol ConstraintValidator {
-    /// Validates a value against a`Constraint` with default values.
+    /// Validates a value against a`Constraint` with default parameters.
     ///
     /// - Parameters:
     ///   - value: A value to be validated.
     /// - Throws: A `ConstraintViolation` error.
     func validate(_ value: Any?) throws
 
-    /// Validates a value against a`Constraint` with custom values.
+    /// Validates a value against a`Constraint` with custom parameters.
     ///
     /// - Parameters:
     ///   - value: A value to be validated.
-    ///   - constraint: An instance of `Constraint`.
+    ///   - constraint: An instance of `Constraint` with custom parameters.
     /// - Throws: A `ConstraintViolation` error.
     func validate(_ value: Any?, against constraint: Constraint) throws
 }
