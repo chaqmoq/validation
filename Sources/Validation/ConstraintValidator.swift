c3e40f447d2e806e41f79c7287c56684183dd501
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-09-27 16:23:05 +0500

    Update documentation for `ConstraintValidator`

--- Sources/Validation/ConstraintValidator.swift
+++ Sources/Validation/ConstraintValidator.swift
@@ -1,17 +1,19 @@
 /// A common protocol for all `Constraint` validators.
 public protocol ConstraintValidator {
-    /// Validates a single primitive `Encodable` value against a `Constraint`with default parameters.
+    /// Validates a primitive `Encodable` value against a `Constraint`with default parameters.
     ///
     /// - Parameters:
-    ///   - value: A single primitive `Encodable` value to be validated.
-    /// - Throws: A `ConstraintViolation` error.
+    ///   - value: A primitive `Encodable` value to be validated.
+    /// - Throws: A `Validator.Error` if the value is not of primitive type or a `ConstraintViolation` if the value doesn't satisfy a
+    /// `Constraint`.
     func validate(_ value: Encodable?) throws
 
-    /// Validates a single primitive `Encodable` value against a`Constraint` with custom parameters.
+    /// Validates a primitive `Encodable` value against a`Constraint` with custom parameters.
     ///
     /// - Parameters:
-    ///   - value: A single primitive `Encodable` value to be validated.
+    ///   - value: A primitive `Encodable` value to be validated.
     ///   - constraint: An instance of `Constraint` with custom parameters.
-    /// - Throws: A `ConstraintViolation` error.
+    /// - Throws: A `Validator.Error` if the value is not of primitive type or a `ConstraintViolation` if the value doesn't satisfy a
+    /// `Constraint`.
     func validate(_ value: Encodable?, against constraint: Constraint) throws
 }
