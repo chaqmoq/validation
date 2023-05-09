2ef9630193427b1e36bf19e5304319c661c3ed67
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-09 12:18:52 +0500

    Make global functions internal

--- Sources/Validation/Extension/Function.swift
+++ Sources/Validation/Extension/Function.swift
@@ -1,10 +1,6 @@
 import Foundation
 
-/// Converts a primitive value to its `String` representation.
-///
-/// - Parameter value: A primitive value.
-/// - Returns: A `String` representation of the value or `nil` if it can't be convert to `String`.
-public func primitive(_ value: Any?) -> String? {
+func primitive(_ value: Any?) -> String? {
     if value == nil {
         return ""
     } else if let value = value as? String {
@@ -46,13 +42,8 @@ public func primitive(_ value: Any?) -> String? {
     return nil
 }
 
-/// Converts a primitive value to its `String` representation or throws a `Validator.Error`.
-///
-/// - Parameter value: A primitive value.
-/// - Throws: A `Validator.Error`  if the value can't be convert to `String`.
-/// - Returns: A `String` representation of the value.
 @discardableResult
-public func assertPrimitive(_ value: Any?) throws -> String {
+func assertPrimitive(_ value: Any?) throws -> String {
     guard let value = primitive(value) else {
         let message = Validator.Error.Message.primitiveValue.text
         throw Validator.Error.invalidArgument(message)
@@ -61,15 +52,8 @@ public func assertPrimitive(_ value: Any?) throws -> String {
     return value
 }
 
-/// Converts a `Constraint` to a certain concrete type or throws a `Validator.Error`.
-///
-/// - Parameters:
-///   - type: A concrete type of `Constraint` to convert to.
-///   - constraint: A `Constraint` to be converted.
-/// - Throws: A `Validator.Error`  if the constraint can't be converted to the certain type.
-/// - Returns: A converted concrete type of `Constraint`.
 @discardableResult
-public func assertConstraintType<T: Constraint>(_ type: T.Type, for constraint: Constraint) throws -> T {
+func assertConstraintType<T: Constraint>(_ type: T.Type, for constraint: Constraint) throws -> T {
     guard let constraint = constraint as? T else {
         let message = Validator.Error.Message.constraintType(type).text
         throw Validator.Error.invalidArgument(message)
