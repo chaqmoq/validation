10c662f3095e05b4bb84c934499ad893fbb01af1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-11 10:29:14 +0500

    Refactor AndConstraintTests

--- Tests/ValidationTests/Constraint/AndConstraintTests.swift
+++ Tests/ValidationTests/Constraint/AndConstraintTests.swift
@@ -3,21 +3,19 @@ import XCTest
 
 final class AndConstraintTests: XCTestCase {
     func testInit() {
-        // Act
-        let constraint = AndConstraint()
-
-        // Assert
-        XCTAssertTrue(constraint.constraints.isEmpty)
-    }
-
-    func testInitWithChildConstraints() {
         // Arrange
         let notBlank = NotBlankConstraint()
         let length = LengthConstraint()
         let groups: Set<Group> = [.default, "custom"]
 
         // Act
-        let constraint = AndConstraint([notBlank, length], groups: groups)
+        var constraint = AndConstraint()
+
+        // Assert
+        XCTAssertTrue(constraint.constraints.isEmpty)
+
+        // Act
+        constraint = AndConstraint([notBlank, length], groups: groups)
 
         // Assert
         XCTAssertEqual(constraint.constraints.count, 2)
