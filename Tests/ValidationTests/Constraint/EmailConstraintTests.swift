13fcfebe1d1ae373849b1a4ba22f2b1190b6d100
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-05-11 10:01:54 +0500

    Refactor EmailConstraintTests and EmailValidatorTests

--- Tests/ValidationTests/Constraint/EmailConstraintTests.swift
+++ Tests/ValidationTests/Constraint/EmailConstraintTests.swift
@@ -3,20 +3,18 @@ import XCTest
 
 final class EmailConstraintTests: XCTestCase {
     func testInit() {
-        // Act
-        let constraint = EmailConstraint()
-
-        // Assert
-        XCTAssertEqual(constraint.message, EmailConstraint.message)
-    }
-
-    func testInitWithCustomMessage() {
         // Arrange
         let message = "This value is not a valid email address."
         let groups: Set<Group> = [.default, "custom"]
 
         // Act
-        let constraint = EmailConstraint(message, groups: groups)
+        var constraint = EmailConstraint()
+
+        // Assert
+        XCTAssertEqual(constraint.message, EmailConstraint.message)
+
+        // Act
+        constraint = EmailConstraint(message, groups: groups)
 
         // Assert
         XCTAssertEqual(constraint.message, message)
--- Tests/ValidationTests/Validator/EmailValidatorTests.swift
+++ Tests/ValidationTests/Validator/EmailValidatorTests.swift
@@ -10,87 +10,34 @@ final class EmailValidatorTests: XCTestCase {
         validator = EmailValidator()
     }
 
-    func testNilValueAgainstImplicitConstraint() {
+    func testInit() {
         // Arrange
-        let value: String? = nil
-
-        // Act/Assert
-        XCTAssertThrowsError(try validator.validate(value)) { error in
-            XCTAssertTrue(error is ConstraintViolation)
-        }
-    }
-
-    func testNilValueAgainstExplicitConstraint() {
-        // Arrange
-        let value: String? = nil
-
-        // Act/Assert
-        XCTAssertThrowsError(try validator.validate(value, against: EmailConstraint())) { error in
-            XCTAssertTrue(error is ConstraintViolation)
-        }
-    }
-
-    func testEmptyValueAgainstImplicitConstraint() {
-        // Arrange
-        let value = ""
-
-        // Act/Assert
-        XCTAssertThrowsError(try validator.validate(value)) { error in
-            XCTAssertTrue(error is ConstraintViolation)
-        }
-    }
-
-    func testEmptyValueAgainstExplicitConstraint() {
-        // Arrange
-        let value = ""
-
-        // Act/Assert
-        XCTAssertThrowsError(try validator.validate(value, against: EmailConstraint())) { error in
-            XCTAssertTrue(error is ConstraintViolation)
-        }
-    }
-
-    func testValueAgainstImplicitConstraint() {
-        // Arrange
+        let nilValue: String? = nil
+        let emptyValue = ""
         let value = "contact@chaqmoq.dev"
+        let invalidValue = "contact.chaqmoq.dev"
 
         // Act/Assert
+        XCTAssertThrowsError(try validator.validate(nilValue)) { error in
+            XCTAssertTrue(error is ConstraintViolation)
+        }
+        XCTAssertThrowsError(try validator.validate(nilValue, against: EmailConstraint())) { error in
+            XCTAssertTrue(error is ConstraintViolation)
+        }
+        XCTAssertThrowsError(try validator.validate(emptyValue)) { error in
+            XCTAssertTrue(error is ConstraintViolation)
+        }
+        XCTAssertThrowsError(try validator.validate(emptyValue, against: EmailConstraint())) { error in
+            XCTAssertTrue(error is ConstraintViolation)
+        }
         XCTAssertNoThrow(try validator.validate(value))
-    }
-
-    func testValueAgainstExplicitConstraint() {
-        // Arrange
-        let value = "contact@chaqmoq.dev"
-
-        // Act/Assert
         XCTAssertNoThrow(try validator.validate(value, against: EmailConstraint()))
-    }
-
-    func testInvalidValueAgainstImplicitConstraint() {
-        // Arrange
-        let value = "contact.chaqmoq.dev"
-
-        // Act/Assert
-        XCTAssertThrowsError(try validator.validate(value)) { error in
+        XCTAssertThrowsError(try validator.validate(invalidValue)) { error in
             XCTAssertTrue(error is ConstraintViolation)
         }
-    }
-
-    func testInvalidValueAgainstExplicitConstraint() {
-        // Arrange
-        let value = "contact@chaqmoq"
-
-        // Act/Assert
-        XCTAssertThrowsError(try validator.validate(value, against: EmailConstraint())) { error in
+        XCTAssertThrowsError(try validator.validate(invalidValue, against: EmailConstraint())) { error in
             XCTAssertTrue(error is ConstraintViolation)
         }
-    }
-
-    func testValueWithInvalidConstraint() {
-        // Arrange
-        let value = "contact.chaqmoq.dev"
-
-        // Act/Assert
         XCTAssertThrowsError(try validator.validate(value, against: IntegerConstraint())) { error in
             XCTAssertTrue(error is Validator.Error)
         }
