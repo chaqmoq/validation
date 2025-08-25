6ef465f30d7a5a369acd111a0a3894b8bd6d55c6
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2025-08-25 22:00:18 +0500

    Fix Locale type inference

--- Sources/Validation/Extension/DateFormatter.swift
+++ Sources/Validation/Extension/DateFormatter.swift
@@ -3,7 +3,7 @@ import Foundation
 extension DateFormatter {
     public static var iso8601: DateFormatter {
         let dateFormatter = DateFormatter()
-        dateFormatter.locale = .init(identifier: "en_US_POSIX")
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
 
         return dateFormatter
