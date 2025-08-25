6f56ee324b57d7710ec5cd99c35eee6a6c6152db
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2025-08-25 21:57:15 +0500

    Replace Self with DateFormatter for proper type inference

--- Sources/Validation/Extension/DateFormatter.swift
+++ Sources/Validation/Extension/DateFormatter.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 extension DateFormatter {
-    public static var iso8601: Self {
-        let dateFormatter = Self()
+    public static var iso8601: DateFormatter {
+        let dateFormatter = DateFormatter()
         dateFormatter.locale = .init(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
 
